98d4bc0b3771d6e9bacf6b345c773f73ac2ea003
NarenderSingh <naren.delhi13@gmail.com>
2019-12-25 13:10:09 +0530

    Initial boiler plate for the project

---
+++ src/components/app/app-component.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import NewComponent from "../news/news-component";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <NewComponent />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/news/news-component.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class NewComponent extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Welcome to React News Portal</h1>
+      </div>
+    );
+  }
+}
+
+export default NewComponent;
